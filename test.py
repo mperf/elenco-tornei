5d9a734e69aef1c205927e2b5047b0fda2f9d32a
Mattia Perfumo <perfumo.m10@gmail.com>
2022-01-27 14:00:10 +0100

    final version

--- test.py
+++ test.py
@@ -8,48 +8,32 @@ from sys import stdin
 
 schools=['Caramuel','Castoldi','Roncalli']
 
-workbook = Workbook()
-sheet = workbook.active
+
 
 for i in range(3):
     cartella = os.path.dirname(os.path.realpath(__file__))
-    myFilesPaths = glob.glob(cartella + '\Elenchi '+schools[i]+'\*.txt')
+    myFilesPaths = glob.glob(cartella + '/Elenchi '+schools[i]+'/*.txt')
+    print(len(myFilesPaths))
+    os.makedirs("results-"+str(schools[i]))
     for j in range(len(myFilesPaths)):
-      f=open(myFilesPaths[j], 'r')
-      index=2
-      sheet["A1"] = "ALUNNI"
-      sheet["C1"] = "BASKET 3 vs 3"
-      sheet["D1"] = "PALLAVOLO MISTA"
-      sheet["E1"] = "CALCETTO"
-      for line in stdin:
-        print(line, end='')
-      for line in f:
-        try:
+
+      with open(myFilesPaths[j]) as f:
+        lines = f.readlines()
+        workbook = Workbook()
+        sheet = workbook.active
+        sheet.column_dimensions['A'].width = 30
+        sheet.column_dimensions['B'].width = 15
+        sheet.column_dimensions['C'].width = 15
+        sheet.column_dimensions['D'].width = 15
+        index=2
+        
+        sheet["A1"] = "ALUNNI "+str(myFilesPaths[j].split(" ")[2].split('.')[0])
+        sheet["B1"] = "BASKET 3 vs 3"
+        sheet["C1"] = "PALLAVOLO MISTA"
+        sheet["D1"] = "CALCETTO"
+        for line in lines:
           value='A'+str(index)
           sheet[value]=line
-        except EOFError:
-          break
-        index+=1
-      workbook.save(filename= myFilesPaths[j][-10:]+".xlsx")
-
-'''
-workbook = Workbook()
-sheet = workbook.active
-
-sheet["A1"] = "ALUNNI"
-sheet["C1"] = "BASKET 3 vs 3"
-sheet["D1"] = "PALLAVOLO MISTA"
-sheet["E1"] = "CALCETTO"
-
-f = open(".txt", "r")
-for x in f:
-  print(x)
-
-
-
-for i in range(35):
-    value='A'+str(i+2)
-    print(value)
-    sheet[value]=
-workbook.save(filename="hello_world.xlsx")
-'''
+          index+=1
+      
+        workbook.save(filename= "results-"+str(schools[i])+"/"+myFilesPaths[j][-8:]+".xlsx")
